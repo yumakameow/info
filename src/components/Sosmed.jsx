c631d53075f114c6b0f286e8a1fe955ee6ff5549
Yumaka Lukaśz <128088905+yumakameow@users.noreply.github.com>
2023-09-18 21:06:41 +0500

    Update Sosmed.jsx

--- src/components/Sosmed.jsx
+++ src/components/Sosmed.jsx
@@ -8,9 +8,9 @@ const Sosmed = ({darkTheme}) => {
   return (
     <div className='flex justify-center items-center text-4xl gap-4 max-w-sm mx-auto text-slate-500 dark:text-slate-400'>
       <a href="https://wa.me/628995101187" className={`hover:scale-105 active:scale-95 transition ${darkTheme ? 'hover:text-slate-200 active:text-white' : 'hover:text-slate-600 active:text-slate-900'}`}> <FaWhatsapp /> </a>
-      <a href="https://www.instagram.com/agengari/" className={`hover:scale-105 active:scale-95 transition ${darkTheme ? 'hover:text-slate-200 active:text-white' : 'hover:text-slate-600 active:text-slate-900'}`}> <AiOutlineInstagram /> </a>
-      <a href="https://www.linkedin.com/in/agengari/" className={`hover:scale-105 active:scale-95 transition ${darkTheme ? 'hover:text-slate-200 active:text-white' : 'hover:text-slate-600 active:text-slate-900'}`}> <AiOutlineLinkedin /> </a>
-      <a href="mailto:agengarinugroho@gmail.com" className={`hover:scale-105 active:scale-95 transition ${darkTheme ? 'hover:text-slate-200 active:text-white' : 'hover:text-slate-600 active:text-slate-900'}`}> <FiMail /> </a>
+      <a href="https://www.instagram.com/yumaka.1337/" className={`hover:scale-105 active:scale-95 transition ${darkTheme ? 'hover:text-slate-200 active:text-white' : 'hover:text-slate-600 active:text-slate-900'}`}> <AiOutlineInstagram /> </a>
+      <a href="https://www.linkedin.com/None" className={`hover:scale-105 active:scale-95 transition ${darkTheme ? 'hover:text-slate-200 active:text-white' : 'hover:text-slate-600 active:text-slate-900'}`}> <AiOutlineLinkedin /> </a>
+      <a href="mailto:hellopipetka@gmail.com" className={`hover:scale-105 active:scale-95 transition ${darkTheme ? 'hover:text-slate-200 active:text-white' : 'hover:text-slate-600 active:text-slate-900'}`}> <FiMail /> </a>
     </div>
   )
 }
