fb52de8fa0db072671725ee9dd7a6dda2dcc65aa
Yumaka Lukaśz <128088905+yumakameow@users.noreply.github.com>
2023-09-18 21:04:31 +0500

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,10 +5,10 @@ export const Modal = ({toggleModal}) => {
     <div>
       <div className="fixed inset-x-0 bottom-0 flex items-center justify-center z-10">
           <div className="bg-white w-full rounded-lg shadow-lg p-6">
-            <h2 className="text-xl font-semibold mb-4">Ini adalah modal</h2>
+            <h2 className="text-xl font-semibold mb-4">Russian Developer</h2>
             <p>Isi konten modal di sini</p>
             <button onClick={toggleModal} className="mt-4 bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold py-2 px-4 rounded">
-              Tutup Modal
+              Live in Moscow
             </button>
           </div>
         </div>
